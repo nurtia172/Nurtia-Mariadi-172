1f1573e1e6fc7ecb809fddc108195516352f5c73
nurtia172 <80872003+nurtia172@users.noreply.github.com>
2021-04-10 20:36:34 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.PenugasaPBO;
+
+public class Main {
+    public static void main(String[] args) {
+        Benda produk = new Benda();
+        produk.menu();
+        produk.getnilaidata();
+    }
+}
